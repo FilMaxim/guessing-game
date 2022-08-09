4eb8be476f8c507e2561d706ffabe3d2c950fdf1
FilMaxim <fil.maxim13@gmail.com>
2022-08-09 09:36:47 +0300

    init: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let a = Math.round((this.min + this.max) / 2);
+        return a
     }
 
     lower() {
-
+        return this.max = Math.round((this.min + this.max) / 2);
     }
 
     greater() {
-
+        return this.min = Math.round((this.min + this.max) / 2);
     }
 }
 
